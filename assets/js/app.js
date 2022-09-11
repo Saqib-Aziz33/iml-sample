c6db390804bcb4977db6d675d271744c48e75211
Saqib-Aziz33 <saqib.aziz41@gmail.com>
2022-09-11 12:10:58 -0700

    basic header responsive

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,6 @@
 const nav = document.getElementById('main-nav')
+const navigation = document.querySelector('.navigation')
+const navToggleBtn = document.querySelector('.nav-open')
 
 window.addEventListener('scroll', function(e){
     if(window.pageYOffset > 200){
@@ -6,4 +8,9 @@ window.addEventListener('scroll', function(e){
     }else{
         nav.classList.remove('sticky-nav')
     }
+})
+
+
+navToggleBtn.addEventListener('click', function(e){
+    navigation.classList.toggle('active')
 })
